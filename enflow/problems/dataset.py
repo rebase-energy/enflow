2e82e09b5561738fab61757cd2abf1837ea1f360
sebaheg <sebastian@rebase.energy>
2024-12-19 13:54:56 +0100

    update dataset

--- enflow/problems/dataset.py
+++ enflow/problems/dataset.py
@@ -10,3 +10,7 @@ class Dataset:
     description: t.Optional[str] = None
     collection: t.Optional[edm.EnergyCollection] = None
     data: t.Optional[t.Dict[str, pd.DataFrame]] = None
+
+    @property
+    def list_data(self):
+        return list(self.data.keys())
